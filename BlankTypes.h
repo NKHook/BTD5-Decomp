cee6d564b7d4fe95b41a31865377878dbb63daee
DisabledMallis <mallisiscool@gmail.com>
2022-07-13 16:18:53 -0400

    Start RE on CBasePositionableObject

---
+++ BlankTypes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdint.h>
+
+#define undefined uint8_t
+#define undefined2 uint16_t
+#define undefined4 uint32_t
+#define undefined8 uint64_t
---
+++ Framework/NewFramework/Graphics/Objects/BasePositionableObject.cpp
@@ -0,0 +1,65 @@
+#include "BasePositionableObject.h"
+
+#include <memory.h>
+
+CBasePositionableObject::CBasePositionableObject() :
+	field1_0x4(0),
+	field2_0x8(0)
+{
+	//Compiler does this automatically
+	//this->field0_0x0 = vftable;
+	this->field3_0xc = 1;
+	this->field6_0x10 = 0;
+	this->field7_0x14 = (undefined4*)&this->field7_0x14;
+	this->field8_0x18 = (undefined4*)&this->field7_0x14;
+	this->field9_0x1c = 0;
+	this->field13_0x20 = 0;
+	this->field14_0x24 = 0;
+	this->field15_0x28 = 0;
+	this->field16_0x2c = 0;
+	this->field17_0x30 = 0;
+	this->field18_0x34 = 0;
+	memset(&this->field19_0x38,0,0x40);
+	this->field70_0x74 = 1.0;
+	this->field53_0x60 = 1.0;
+	this->field36_0x4c = 1.0;
+	this->field19_0x38 = 1.0;
+	this->field71_0x78 = 1;
+	this->field74_0x7c = 0;
+	this->field75_0x80 = 0;
+	this->field76_0x84 = 0;
+	this->field77_0x88 = 9;
+	this->field79_0x90 = 0;
+	this->field80_0x94 = 0;
+	this->field81_0x98 = 0;
+	this->field82_0x9c = 0;
+	this->field83_0xa0 = 1.0;
+	this->field84_0xa4 = 1.0;
+	this->field78_0x8c = 0;
+	//Done by compiler automatically
+	//return this;
+}
+undefined CBasePositionableObject::FUN_008b1c20() {}
+undefined CBasePositionableObject::FUN_005a6050() {}
+undefined CBasePositionableObject::FUN_00668ac0() {}
+undefined CBasePositionableObject::FUN_009e6e40(undefined4 param_2) {}
+undefined CBasePositionableObject::FUN_009e6d60() {}
+undefined CBasePositionableObject::FUN_00645b40(undefined4 param_2) {}
+undefined CBasePositionableObject::FUN_00645b90() {}
+undefined CBasePositionableObject::FUN_00645c10() {}
+undefined CBasePositionableObject::FUN_00645cc0(undefined4 param_2) {}
+undefined CBasePositionableObject::FUN_00645d70() {}
+undefined CBasePositionableObject::FUN_00645e60(undefined4 param_2) {}
+undefined CBasePositionableObject::FUN_00645eb0() {}
+undefined CBasePositionableObject::FUN_00645f30() {}
+undefined CBasePositionableObject::FUN_00645fb0() {}
+undefined CBasePositionableObject::FUN_00646030() {}
+undefined CBasePositionableObject::FUN_00646050(undefined4 param_2) {}
+undefined CBasePositionableObject::FUN_006460a0() {}
+undefined CBasePositionableObject::FUN_006460b0() {}
+undefined CBasePositionableObject::FUN_006460d0() {}
+undefined CBasePositionableObject::FUN_00646110() {}
+undefined CBasePositionableObject::FUN_009e6a10() {}
+undefined CBasePositionableObject::_guard_check_icall() {}
+undefined CBasePositionableObject::FUN_009e69d0() {}
+undefined CBasePositionableObject::FUN_009e6bc0() {}

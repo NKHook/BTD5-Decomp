fa4a8293b6839038d99a40d18b0f75f6e87aeaf0
Mike <mallisiscool@gmail.com>
2022-07-20 02:05:35 -0400

    CLoc class & constructor

--- Framework/NewFramework/Loc/Loc.h
+++ Framework/NewFramework/Loc/Loc.h
@@ -2,6 +2,21 @@
 
 #include <string>
 
+enum class Texts {
+    LOC_NULL = 0x0,
+    LOC_DECIMAL = 0x1,
+    LOC_DELIMITER = 0x2,
+    LOC_MM_Chest_ComeBack = 0x27,
+    LOC_MM_BUILDING_Temple = 0x31,
+    LOC_MM_BUILDING_Settings = 0xA9,
+    LOC_SETTINGS_Controls = 0xAB,
+    LOC_SETTINGS_DragDrop = 0xAC,
+    LOC_SETTINGS_DropLock = 0xAD,
+    LOC_SETTINGS_Language = 0xAE,
+    LOC_SETTINGS_Copyright = 0xAF,
+    LOC_MISC_LimitedTime = 0x19B
+};
+
 class CLoc {
     //Ignore first 4 bytes bc vtable
 	//char pad_0000[4]; //0x0000

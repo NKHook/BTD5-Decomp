1e96957ab4e5a17085a28dde8ad598503ae0a710
Mike <mallisiscool@gmail.com>
2022-07-24 22:50:14 -0400

    Scaffolding for FlagStringConverter

---
+++ Framework/NewFramework/Utilities/FlagStringConverter.cpp
@@ -0,0 +1,11 @@
+#include "FlagStringConverter.h"
+
+CategoryStruct* CFlagStringConverter::GetCategory(uint32_t categoryId) {
+    return nullptr;
+}
+std::string* CFlagStringConverter::FlagToString(std::string* result, uint32_t category, uint64_t flag) {
+    return nullptr;
+}
+uint64_t CFlagStringConverter::StringToFlag(uint32_t category, std::string* result) {
+    return nullptr;
+}
